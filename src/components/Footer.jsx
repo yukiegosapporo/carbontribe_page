16284ffe3443518b3d48eef1d5fbb38128727cc8
Yuki Katoh <yukiegosapporo@gmail.com>
2023-01-23 12:00:25 +0100

    logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { FacebookIcon } from "../assets/icons/FacebookIcon";
 import { InstagramIcon } from "../assets/icons/InstagramIcon";
-import { TailcastLogo } from "../assets/logos/TailcastLogo";
+import { Logo } from "../assets/logos/Logo";
 import { TwitterIcon } from "../assets/icons/TwitterIcon";
 
 export const Footer = () => {
@@ -12,7 +12,7 @@ export const Footer = () => {
             <div className="w-full lg:w-1/3 mb-16 lg:mb-0">
               <div className="flex justify-center lg:justify-start items-center grow basis-0">
                 <div className="text-white mr-2 text-6xl">
-                  <TailcastLogo />
+                  <Logo />
                 </div>
                 <div className="text-white font-['Inter'] font-bold text-xl">
                   Carbontribe
--- src/components/InvitationModal.jsx
+++ src/components/InvitationModal.jsx
@@ -2,7 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 import { CheckArrowIcon } from "../assets/icons/CheckArrowIcon";
 import { CloseIcon } from "../assets/icons/CloseIcon";
-import { TailcastLogo } from "../assets/logos/TailcastLogo";
+import { Logo } from "../assets/logos/Logo";
 
 export const InvitationModal = ({ setIsOpen }) => (
   <AnimatePresence>
@@ -47,10 +47,10 @@ export const InvitationModal = ({ setIsOpen }) => (
             <div className="w-full lg:w-1/2 flex items-center flex-col justify-center pt-24 sm:pt-0">
               <div className="flex inline lg:hidden justify-start items-center grow basis-0 mb-8 pr-6">
                 <div className="text-white mr-2 text-8xl">
-                  <TailcastLogo />
+                  <Logo />
                 </div>
                 <div className="text-white font-['Inter'] font-bold text-3xl">
-                  Tailcast
+                  Carbontribe
                 </div>
               </div>
 

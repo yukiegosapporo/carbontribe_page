bc6709ddd3a4d36fa4016a24557819bb9f81d698
yuki <yuki@yukis-MacBook-Air.local>
2024-02-23 22:16:28 -0300

    deleted archive

--- archive/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  site: 'https://yukiegosapporo.github.io',
-  // base: '/carbontribe_page',
-  integrations: [react(), tailwind()]
-});
--- archive/src/assets/logos/logo.jsx
+++
@@ -1,5 +0,0 @@
-import logo_png from "../images/logo.png";
-
-export const Logo = () => (
-  <img src={logo_png} alt="logo" width="80px"/>
-);
